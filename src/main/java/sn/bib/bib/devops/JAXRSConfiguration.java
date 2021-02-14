f26a8d805a873ea2411e957dd69bf8288d443b29
BiB <habibndiaye08@gmail.com>
2021-02-14 04:19:27 +0000

    Init commit

---
+++ src/main/java/sn/bib/bib/devops/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package sn.bib.bib.devops;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
